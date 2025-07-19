20b3a0f2a9e598e216d98850429affe38bb3b8e3
sooryatry <sooryahari535@gmail.com>
2025-07-19 10:42:16 +0530

    learn3

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
+const path = require("path");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
@@ -15,8 +17,17 @@ const db = mysql.createConnection({
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//makes the frontend codes display on website
+//makes the frontend codes in public directory display on website
 app.use(express.static("public")); // for serving static HTML, CSS, JS
+
+//for session ie. for making sure u cannot acess homwpage without login
+app.use(
+  session({
+    secret: "myscretkey",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 //for sql connection testing
 db.connect((err) => {
@@ -25,6 +36,21 @@ db.connect((err) => {
   } else {
     console.log("connected to  mysql database");
   }
+});
+
+//for making the .html in url go away
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/homepage", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "homepage.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "login.html"));
+});
+
+app.get("/signup", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "signup.html"));
 });
 
 // Routes
@@ -43,7 +69,7 @@ app.post("/signup", (req, res) => {
       res.send("signup failed");
     } else {
       console.log("user registered:", results.insertid);
-      res.send("signup sucessfull");
+      res.redirect("/login");
     }
   });
 });
@@ -60,16 +86,35 @@ app.post("/login", (req, res) => {
     } else if (results.length === 0) {
       res.send("user not found");
     } else {
+      // req.session.user = results[0];
+
       const user = results[0];
+
       if (user.password === password) {
+        //cannot have send and redirect at the same time
         // res.send(`welcome back, ${user.name}`);
-        res.redirect("/public/homepage.html");
+        res.redirect("/homepage");
       } else {
         res.send("incorrect password");
       }
     }
   });
 });
+
+// app.get("/homepage", (req, res) => {
+//   if (req.session.user) {
+//     res.sendFile(path.join(__dirname, "public", "homepage.html"));
+//   } else {
+//     res.redirect("/");
+//   }
+// });
+
+// app.get('/logout',(req,res)=>{
+//   req.session.destroy(err=>{
+//     if(err) throw err;
+//     res.redirect("/")
+//   })
+// })
 
 // Start server
 app.listen(port, () => {
